a608116d5afafe7c43578448204b229a1832b173
Francois Botha <botha.francois@gmail.com>
2023-12-22 22:38:38 +1100

    Style Contact Form

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -1,50 +1,75 @@
 <template>
+  <section id="contact-banner">
+    <h1 class="contact-banner__title">Contact</h1>
+  </section>
 
-    <section id="contact-banner">
-        <h1 class="contact-banner__title">Contact</h1>
-    </section>
-
-    <div class="contact-container">
-      <div class="contact-form">
-        <!-- <div class="info">
-          <div class="email">Email: hey@francoisbotha.io</div>
+  <section id="contact-form-section">
+    <div class="contact-container-grid">
+      <div class="col-1">
+      </div>
+      <div class="col-2">
+        <div>
+          <div class="info">
+          <p></p>
+          <div class="email-label">Email: </div>
+          <div class="email-text">hey@francoisbotha.io </div>
+          
+          <p></p>
           <div class="social-media">
             Social Media:
-            <a href="#" class="icon">LinkedIn</a>
-            <a href="#" class="icon">TikTok</a>
-            <a href="#" class="icon">Instagram</a>
-            <a href="#" class="icon">YouTube</a>
+            <p>
+              <Icon name="entypo-social:github-with-circle" size="1.5rem" class="icon" />
+              <Icon name="entypo-social:twitter" size="1.5rem" class="icon" />
+              <Icon name="entypo-social:linkedin-with-circle" size="1.5rem" class="icon" />
+            </p>
           </div>
-        </div> -->
-        <form action="index.html" class="contact-form">
-            <label for="first-name">First Name</label>
-            <input type="text" id="first-name">
-            <label for="last-name">Last Name</label>
-            <input type="text" id="last-name">
-            <label for="email">Email</label>
-            <input type="email" id="email" placeholder="Email *" />
-            <label for="message">Message</label>
-            <textarea placeholder="Message" id="message"></textarea>
-            <button type="submit" class="button">Send</button>
-        </form>
+        </div>
+        </div>
+
+      </div>
+      <div class="col-3">
+        <div >
+          <form action="" class="contact-form">
+            <p>
+              <label for="first-name">First Name</label>
+              <input type="text" id="first-name">
+            </p>
+            <p>
+              <label for="last-name">Last Name</label>
+              <input type="text" id="last-name">
+            </p>
+            <p class="full-width">
+              <label for="email">Email</label>
+              <input type="email" id="email"  />
+            </p>
+            <p class="full-width">
+              <label for="">Message</label>
+              <textarea name="" id="" cols="30" rows="7"></textarea>
+            </p>
+            <p class="full-width">
+              <button type="submit" class="button">Send</button>
+            </p>
+          </form>
       </div>
     </div>
-  </template>
+    <div class="col-4">
+    </div>
+    </div>
+  </section>
+</template>
   
-  <script>
-  export default {
-    name: 'ContactPage',
-    methods: {
-      submitForm() {
-        // Form submission logic goes here
-        console.log('Form submitted');
-      },
+<script>
+export default {
+  name: 'ContactPage',
+  methods: {
+    submitForm() {
+      // Form submission logic goes here
+      console.log('Form submitted');
     },
-  };
-  </script>
+  },
+};
+</script>
   
-  <style scoped>
-
-  @import url("~/assets/css/contact.css");
-
-  </style>
+<style scoped>
+@import url("~/assets/css/contact.css");
+</style>
